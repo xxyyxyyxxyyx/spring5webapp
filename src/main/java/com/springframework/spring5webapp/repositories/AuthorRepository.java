0c7fb0c846c872147cf2cbcfa8713b902019efd5
Administrator <xxyyxyyxxyyx@gmail.com>
2017-10-11 10:48:01 +0800

    Spring Data JPA Repositories

---
+++ src/main/java/com/springframework/spring5webapp/repositories/AuthorRepository.java
@@ -0,0 +1,7 @@
+package com.springframework.spring5webapp.repositories;
+
+import com.springframework.spring5webapp.model.Author;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+}
